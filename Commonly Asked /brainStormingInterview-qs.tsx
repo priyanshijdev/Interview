8cbc7ee59b904a4777c6ab4f07970395cee68e44
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-04-01 22:16:45 +0530

    Create brainStormingInterview-qs.tsx

---
+++ Commonly Asked /brainStormingInterview-qs.tsx
@@ -0,0 +1,7 @@
+ Which things cause React components to re-render?
+- What is useEffect, and what is a case where you might want to use it?
+- How can useCallback solve a common issue with JavaScript in React?
+- What is React Context and how does it work?
+- When would you want to use the useMemo hook?
+- How do you decide how to structure your components?
+
