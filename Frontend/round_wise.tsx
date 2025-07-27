a435874cc3dfc3f89b8088e03563186111349c03
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-07-27 22:51:54 +0530

    Create round_wise.tsx

---
+++ Frontend/round_wise.tsx
@@ -0,0 +1,49 @@
+Topics Covered in Round 1 (With SDE/SDE-2)
+  Frontend Fundamentals
+  ● Core concepts of HTML, including structure and best practices.
+  ● Styling techniques using CSS, layout strategies, and responsive design.
+  ● JavaScript fundamentals, including execution flow, scope, and memory management.
+  React.js
+  ● Understanding component behavior and state management.
+  ● Lifecycle methods and hooks for managing component updates.
+  ● Best practices for structuring a React application.
+  Problem-Solving & Implementation
+  ● Hands-on coding challenges using APIs and UI rendering.
+  ● Building interactive features with state and event handling.
+  Preparation Tips
+  ● Refresh core frontend development concepts.
+  ● Be comfortable coding in an online environment.
+  ● Focus on writing clean and efficient code.
+
+
+Topics Covered in Round 2 (Deep Dive With Tech Lead)
+  Frontend Advanced
+  ● Core concepts of JS, including structure and best practices.
+  ● Advanced JavaScript, including working examples, the interviewer can ask you to
+  implement small problem statements. (Example: Event delegation, event handlers,
+  prototypes etc..)
+  React.js
+  ● Optimization techniques for improving performance.
+  ● State management techniques could be asked, can also ask to implement some feature
+  in react.
+  ● Interviewer will also note your code quality and code structure, which design patterns are
+  you writing, for your applications
+  Problem-Solving & Implementation
+  ● Hands-on coding challenges using APIs, UI rendering, Optimizations in frontend
+  application.
+  ● Building interactive features with state and event handling.
+  Preparation Tips
+  ● Refresh core frontend development concepts.
+  ● Be comfortable coding in an online environment (code sandbox preferably).
+  ● Focus on writing clean and efficient code.
+
+
+Round 3 Interview (With CTO / Hiring Manager)
+  Web Fundamentals & Advanced Concepts
+  ● In-depth discussion on core web technologies and advanced concepts.
+  ● Expect questions related to technologies you've worked with in past projects.
+  ● Topics may include advanced web principles, Git methodologies, and best practices.
+  Problem-Solving & Implementation
+  ● Hands-on coding challenges will be included.
+  ● Be prepared for DSA problems, particularly on Arrays, Objects, Linked Lists, and
+  Recursion
